1d40eb69b63e7d9f9ca6cfcfaaeec32237d368c2
Dan <justlivedan@live.com>
2023-08-23 20:03:52 -0600

    Test

--- Unity/Showcase/App/Assets/App/List/RemoteObjectListLoader.cs
+++ Unity/Showcase/App/Assets/App/List/RemoteObjectListLoader.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using Microsoft.Azure.Storage;
+﻿using Microsoft.Azure.Storage;
 using Microsoft.MixedReality.Toolkit.Extensions;
 using System;
 using System.Collections;
